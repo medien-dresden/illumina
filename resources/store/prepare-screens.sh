71c4149d1e53cf8f5ef592a3364be9e009b9d892
Peter Heisig <peter.heisig@medien-dresden.de>
2014-02-08 12:38:50 +0100

    control screens revised

--- resources/store/prepare-screens.sh
+++ resources/store/prepare-screens.sh
@@ -1,7 +1,8 @@
-#!/bin/bash
-
 # resize image for HTC One S
 mogrify -resize 480x818 screen*.png
 
 # remove 18px at the bottom
 mogrify -chop 0x18+0+0 -gravity South screen*.png
+
+# get screen
+adb shell screencap -p | sed 's/\r$//' > screen.png
